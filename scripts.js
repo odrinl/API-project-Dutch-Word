8d32dbfddfb0aa9132a49703dad912490f6c23cb
odrinl <keozon@gmail.com>
2023-07-14 19:09:34 +0200

    adding pictures for the search

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 document.getElementById('translate-button').addEventListener('click', translate);
 
 function translate() {
@@ -11,6 +12,8 @@ function translate() {
     targetLanguages.forEach(function(targetLang) {
         translateText(sourceText, sourceLang, targetLang);
     });
+    // Call the function to fetch and display related images
+    fetchImages(sourceText);
 }
 
 function translateText(sourceText, sourceLang, targetLang) {
@@ -22,8 +25,34 @@ function translateText(sourceText, sourceLang, targetLang) {
         .then(response => response.json())
         .then(data => {
             // Get the corresponding element based on the target language
-            let translatedWordElement = document.getElementById('translated-word-' + targetLang);
+            let translatedWordElement = document.getElementById(`translated-word-${targetLang}`);
             translatedWordElement.textContent = data[0][0][0];
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function fetchImages(query) {
+    const API_KEY = '38259307-8733456e700ed630a3379faf0';
+    const perPage = 5;
+    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+        query
+    )}&lang=nl&image_type=photo&safesearch=true&per_page=${perPage}`;
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            if (data.hits.length > 0) {
+                const imagesContainer = document.getElementById('images-container');
+                imagesContainer.innerHTML = ''; // Clear existing content
+
+                data.hits.forEach(hit => {
+                    const imgElement = document.createElement('img');
+                    imgElement.src = hit.webformatURL;
+                    imgElement.alt = hit.tags;
+                    imgElement.classList.add('image');
+                    imagesContainer.appendChild(imgElement);
+                });
+            }
         })
         .catch(error => console.error('Error:', error));
 }
